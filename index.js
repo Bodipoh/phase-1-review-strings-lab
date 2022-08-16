a6f066cb15390421a68e2107db419727083e1f80
+ <+>
2022-08-17 01:02:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Tie Got';
+const welcomeMessage = 'Welcome to Flatbook, Tie Got!';
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+
+const firstInitial = 'Tie Got[0]';
+const restOfName = 'Tie Got.slice(1)';
+
+const shortGreeting = 'Welcome, Tie Got!';
